d9c1c315a9cd858714fe824e8c66512546570203
Eenok Randlepp <eenok@randlepp.ee>
2026-05-22 12:51:41 +0000

    Lisatud GitHub Pages seadistus ja config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://eenokr.github.io',
+	base: '/keila.advent.ee',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
