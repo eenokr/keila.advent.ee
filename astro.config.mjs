0be62b85bebcebe4543d9911d656cd2d16881134
Eenok Randlepp <eenok@randlepp.ee>
2026-05-23 20:48:14 +0000

    remove fonts config, use css import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,9 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://keila-advent-ee.vercel.app',
 	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.google(),
-			name: 'Atkinson Hyperlegible',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-		},
-	],
 });
