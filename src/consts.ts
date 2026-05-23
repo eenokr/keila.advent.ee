4dab566adfb7faf3d369473a88760c7954c9e2a0
Eenok Randlepp <eenok@randlepp.ee>
2026-05-24 01:24:43 +0300

    codex test

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Keila Adventkogudus';
+export const SITE_DESCRIPTION = 'Keila Adventkoguduse koduleht ja jumalateenistuste info.';
