b09f6e3bfdbeca6424ff403d8e9a8b8c0a656072
Eenok Randlepp <eenok@randlepp.ee>
2026-05-26 01:07:42 +0300

    oma domeen

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import { base } from './vite.config.js';
-
-const owner = process.env.GITHUB_REPOSITORY_OWNER;
 
 export default defineConfig({
-	site: owner ? `https://${owner}.github.io` : 'https://keila.advent.ee',
-	base,
+	site: 'https://keila.randlepp.ee',
+	base: '/',
 	output: 'static',
 	integrations: [mdx(), sitemap()],
 });
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
-const isProjectPage = repoName && !repoName.endsWith('.github.io');
-
-export const base = process.env.GITHUB_ACTIONS && isProjectPage ? `/${repoName}/` : '/';
-
 export default defineConfig({
-	base,
+	base: '/',
 });
